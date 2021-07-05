d0204913d71069b5bce6bb72b24c935d2cbfbd2b
Daniel <daniel.rasmussen@drgeo.se>
2021-07-05 18:19:33 +0200

    updates

--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -10,19 +10,19 @@ class Buttons extends React.Component {
         return (
             <div className={className}>
                 <h1 id="test">{test}</h1>
-                <button id="seven" onClick={this.props.numbers} value="7">
+                <button id="seven" onClick={this.props.decimal} value="7">
                     7
                 </button>
-                <button id="eight" onClick={this.props.numbers} value="8">
+                <button id="eight" onClick={this.props.decimal} value="8">
                     8
                 </button>
-                <button id="nine" onClick={this.props.numbers} value="9">
+                <button id="nine" onClick={this.props.decimal} value="9">
                     9
                 </button>
                 <button
                     id="divide"
                     className={[className, 'operator'].join(" ")}
-                    onClick={this.props.initialize}
+                    onClick={this.props.operators}
                     value="/"
                 >
                     /
@@ -35,41 +35,41 @@ class Buttons extends React.Component {
                 >
                     C
                 </button>
-                <button id="four" onClick={this.props.numbers} value="4">
+                <button id="four" onClick={this.props.decimal} value="4">
                     4
                 </button>
-                <button id="five" onClick={this.props.numbers} value="5">
+                <button id="five" onClick={this.props.decimal} value="5">
                     5
                 </button>
-                <button id="six" onClick={this.props.numbers} value="6">
+                <button id="six" onClick={this.props.decimal} value="6">
                     6
                 </button>
                 <button
                     id="multiply"
                     className={[className, 'operator'].join(" ")}
-                    onClick={this.props.initialize}
+                    onClick={this.props.operators}
                     value="x"
                 >
                     x
                 </button>
-                <button id="one" onClick={this.props.numbers} value="1">
+                <button id="one" onClick={this.props.decimal} value="1">
                     1
                 </button>
-                <button id="two" onClick={this.props.numbers} value="2">
+                <button id="two" onClick={this.props.decimal} value="2">
                     2
                 </button>
-                <button id="three" onClick={this.props.numbers} value="3">
+                <button id="three" onClick={this.props.decimal} value="3">
                     3
                 </button>
                 <button
                     id="subtract"
                     className={[className, 'operator'].join(" ")}
-                    onClick={this.props.initialize}
+                    onClick={this.props.operators}
                     value="-"
                 >
                     -
                 </button>
-                <button id="zero" onClick={this.props.numbers} value="0">
+                <button id="zero" onClick={this.props.decimal} value="0">
                     0
                 </button>
                 <button id="decimal" onClick={this.props.decimal} value=".">
@@ -78,7 +78,7 @@ class Buttons extends React.Component {
                 <button
                     id="percent"
                     className={[className, 'operator'].join(" ")}
-                    onClick={this.props.initialize}
+                    onClick={this.props.operators}
                     value="%"
                 >
                     %
@@ -86,7 +86,7 @@ class Buttons extends React.Component {
                 <button
                     id="add"
                     className={[className, 'operator'].join(" ")}
-                    onClick={this.props.initialize}
+                    onClick={this.props.operators}
                     value="+"
                 >
                     +
@@ -94,7 +94,7 @@ class Buttons extends React.Component {
                 <button
                     id="equals"
                     className={[className, 'operator'].join(" ")}
-                    onClick={this.props.initialize}
+                    onClick={this.props.operators}
                     value="="
                 >
                     =
--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -9,22 +9,31 @@ class Calculator extends React.Component {
     static id = "Calculator";
     static className = "app";
     static test = "This is a Calculator";
+    static limitText = 'Limit Met!';
+    static limitInt = 21;
+    static isOperator = /[x/+‑]/;
+    static isDecimal = /([^.0-9]0|^0)$/;
 
     constructor(props) {
         super(props);
-        this.state = {
+
+        this
+            .state = {
             currentValue: '0',
             previousValue: '0',
+            history: [],
             formula: '0'
         }
-        this.maxWarn = this.maxWarn.bind(this);
         this.initialize = this.initialize.bind(this);
+        this.handleNumbers = this.handleNumbers.bind(this);
+        this.maxWarn = this.maxWarn.bind(this);
     }
 
     maxWarn() {
-        const { state: { currentValue, previousValue}} = this;
+        const {state: {currentValue, previousValue}} = this;
+        const {limitText} = Calculator;
         this.setState({
-            currentValue: 'LIMIT MET!',
+            currentValue: limitText,
             previousValue: currentValue
         });
         setTimeout(() => this.setState({
@@ -32,16 +41,44 @@ class Calculator extends React.Component {
         }), 1000)
     }
 
+    handleNumbers(e) {
+        const _ = this;
+        const {limitText, limitInt} = Calculator;
+        const {state: {currentValue, evaluated}} = _;
+        if (currentValue.includes(limitText) === false) {
+            const {target: {value: currentValue}} = e;
+            const {length} = currentValue;
+            _.setState({evaluated: false})
+            if (length > limitInt) {
+                _.maxWarn();
+            } else if (evaluated) {
+                _.setState({
+                    currentValue,
+                    formula: currentValue !== '0' ? currentValue : ''
+                });
+            } else {
+                // const {isOperator, isDecimal} = Calculator;
+                _.setState({
+                    currentValue,
+                    formula: currentValue
+                })
+            }
+        }
+    }
+
     initialize() {
         this.setState({
             currentValue: '0',
-            formula: ''
+            previousValue: '0',
+            history: [],
+            formula: '',
+            evaluated: false
         })
     }
 
     render() {
         const {id, test, className} = Calculator;
-        const {state: {currentValue, formula}, initialize} = this;
+        const {state: {currentValue, formula}, initialize, handleNumbers} = this;
         return (
             <div id={id} className={className}>
                 <h1 id="test">{test}</h1>
@@ -49,6 +86,7 @@ class Calculator extends React.Component {
                 <Display currentValue={currentValue}/>
                 <Buttons
                     initialize={initialize}
+                    decimal={handleNumbers}
                 />
                 <Author/>
             </div>
