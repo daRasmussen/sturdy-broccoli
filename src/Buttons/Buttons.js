5a30ff11390b3fadb6efac957e4a53524e9294d3
Daniel <daniel.rasmussen@drgeo.se>
2021-07-05 07:33:11 +0200

    updates

--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './scss/Buttons.scss';
 
 class Buttons extends React.Component {
     #author = 'drGeo'
@@ -14,13 +15,14 @@ class Buttons extends React.Component {
         return (
             <div className={className}>
                 <h1 id="test">{test}</h1>
-                <button
-                    id="clear"
-                    className={[className, 'ac'].join(" ")}
-                    onClick={this.props.initialize}
-                    value="AC"
-                >
-                    AC
+                <button id="seven" onClick={this.props.numbers} value="7">
+                    7
+                </button>
+                <button id="eight" onClick={this.props.numbers} value="8">
+                    8
+                </button>
+                <button id="nine" onClick={this.props.numbers} value="9">
+                    9
                 </button>
                 <button
                     id="divide"
@@ -30,6 +32,15 @@ class Buttons extends React.Component {
                 >
                     /
                 </button>
+                <button
+                    id="clear"
+                    className={[className, 'operator'].join(" ")}
+                    onClick={this.props.initialize}
+                    value="C"
+                >
+                    C
+                </button>
+
                 <button
                     id="multiply"
                     className={[className, 'operator'].join(" ")}
